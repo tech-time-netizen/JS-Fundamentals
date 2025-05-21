d0653aba2a71dd7ee0537814e33eba089d8c1670
tech-time-netizen <e2476168@gmail.com>
2025-05-21 21:41:33 +0300

    Arguments

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args = process.argv.slice(2); // Remove first two default arguments
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
+  console.log("Arguments found");
+}
